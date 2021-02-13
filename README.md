1b8a498d040dc13b5bf767f7f6da48e52ff8d6e3
Jialin <a9k5rzz@mmm.com>
2021-02-12 21:37:05 -0600

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+## Usage
+
+1. `pip install selenium`
+2. download chrome driver from [https://sites.google.com/a/chromium.org/chromedriver/downloads](https://sites.google.com/a/chromium.org/chromedriver/downloads)
+3. Place it into the current directory
+4. Run [find_parking_selenium.py](find_parking_selenium.py)
---
+++ find_parking_selenium.py
@@ -0,0 +1,41 @@
+import datetime, time
+import os
+
+from PIL import Image
+import numpy as np
+from selenium import webdriver
+
+interval = 30
+screenshot = None
+driver = webdriver.Chrome(executable_path='./chromedriver')
+driver.get("https://www.snowbird.com/parking/#parking_reservation")
+n = 0
+while True:
+    try:
+        print(datetime.datetime.now())
+
+        driver.implicitly_wait(8)
+        elem1 = driver.find_element_by_xpath('//iframe')
+        driver.switch_to.frame(elem1)
+
+        elem2 = driver.find_element_by_xpath('//div[2]/div/table/tbody/tr[3]/td[5]/div')
+        elem2.screenshot(f'screenshot_{n}.png')
+        im_frame = Image.open(f'screenshot_{n}.png')
+        current_screenshot = np.array(im_frame)
+
+        if screenshot is None:
+            screenshot = current_screenshot.copy()
+        else:
+            img_diff = np.sum(screenshot.flatten() - current_screenshot.flatten()) / np.sum(current_screenshot.flatten())
+            if img_diff > 0.05:
+                os.system('say "Find new spot!"')
+                elem2.screenshot('new_spot.png')
+            else:
+                os.remove(f'screenshot_{n-1}.png')
+        driver.refresh()
+        n += 1
+    except Exception as e:
+        print(e)
+        os.system('say "something is wrong"')
+
+    time.sleep(interval)
